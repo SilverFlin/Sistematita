c1a9f61248fdb48bab68b377c1b853ac218ea295
SilverFlin <toledorusso@outlook.com>
2023-11-30 12:01:07 -0700

    chore: update maceta.ino

--- maceta/maceta.ino
+++ maceta/maceta.ino
@@ -8,15 +8,21 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <NewPing.h>
+#include <NoDelay.h>
 
 #define sensor 33
 #define relay 4
 
 const int DISTANCIA_ALERTA = 30;
 
+const long TIEMPO_TAREA = 500;
+
+
 // Configuramos los pines del sensor Trigger y Echo
 const int PinTrig = 5;
 const int PinEcho = 18;
+
+noDelay pausa(TIEMPO_TAREA);
 
 // Crea una instancia de la clase NewPing
 NewPing sonar(PinTrig, PinEcho, DISTANCIA_ALERTA);
@@ -30,17 +36,19 @@ BlynkTimer timer;
 char auth[] = "GwQJiB-WDTRKg5tneK2WwmPOLvEDh6Z7";
 
 //Enter your WIFI SSID and password
-char ssid[] = "Redmi 10C";
-char pass[] = "123456789a";
+char ssid[] = "plantita";
+char pass[] = "12345678";
 
 int obtenDistancia();
 
 void setup() {
   // Debug console
   Serial.begin(115200);
-  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  lcd.init();
-  lcd.backlight();
+  Serial.print("aaaaaa");
+
+  // Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  // lcd.init();
+  // lcd.backlight();
   pinMode(relay, OUTPUT);
   digitalWrite(relay, HIGH);
   // Ponemos el pin Trig en modo salida
@@ -48,15 +56,15 @@ void setup() {
   // Ponemos el pin Echo en modo entrada
   pinMode(PinEcho, INPUT);
 
-  Serial.print("aaaaaa");
-  lcd.setCursor(1, 0);
-  lcd.print("System Loading");
-  for (int a = 0; a <= 15; a++) {
-    lcd.setCursor(a, 1);
-    lcd.print(".");
-    delay(200);
-  }
-  lcd.clear();
+
+  // lcd.setCursor(1, 0);
+  // lcd.print("System Loading");
+  // for (int a = 0; a <= 15; a++) {
+  //   lcd.setCursor(a, 1);
+  //   lcd.print(".");
+  //   delay(200);
+  // }
+  // lcd.clear();
 }
 
 void medirNivelAgua() {
@@ -66,9 +74,11 @@ void medirNivelAgua() {
   // Calcular la distancia a la que se encuentra el objeto
   int distancia = sonar.convert_cm(uS);
   //if(distancia==DISTANCIA_ALERTA){
-   // Serial.print("Rellenar tanque");
+  // Serial.print("Rellenar tanque");
   //}
-  Serial.print(distancia);
+  Serial.println(distancia);
+  Serial.print("< Echo");
+  digitalWrite(relay, HIGH);
 }
 
 //Get the ultrasonic sensor values
@@ -76,12 +86,10 @@ void soilMoisture() {
   int value = analogRead(sensor);
   value = map(value, 0, 4095, 0, 100);
   value = (value - 100) * -1;
-  Blynk.virtualWrite(V0, value);
+  // Blynk.virtualWrite(V0, value);
   Serial.println(value);
-  lcd.setCursor(0, 0);
-  lcd.print("Moisture :");
-  lcd.print(value);
-  lcd.print(" ");
+  Serial.print("< agua");
+  digitalWrite(relay, LOW);
 }
 
 //Get the button value
@@ -89,8 +97,8 @@ BLYNK_WRITE(V1) {
   bool Relay = param.asInt();
   if (Relay == 1) {
     digitalWrite(relay, LOW);
-    lcd.setCursor(0, 1);
-    lcd.print("Motor is ON ");
+    // lcd.setCursor(0, 1);
+    // lcd.print("Motor is ON ");
   } else {
     digitalWrite(relay, HIGH);
     lcd.setCursor(0, 1);
@@ -99,9 +107,14 @@ BLYNK_WRITE(V1) {
 }
 
 void loop() {
-  soilMoisture();
-  medirNivelAgua();
-  Blynk.run();  //Run the Blynk library
+  if (pausa.update()) {
+    soilMoisture();
+  } else {
+    medirNivelAgua();
+  }
 
-  delay(200);
+
+  // Blynk.run();  //Run the Blynk library
+
+  // delay(5000);
 }
