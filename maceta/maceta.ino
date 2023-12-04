ac228d6fe82b81129208be79156a3f6a90dca6aa
Sp00kyDucklyn <hoshi.saga.dokuringo@gmail.com>
2023-12-03 21:19:05 -0700

    Finalización código circuito

--- maceta/maceta.ino
+++ maceta/maceta.ino
@@ -3,7 +3,6 @@
 #define BLYNK_AUTH_TOKEN "GwQJiB-WDTRKg5tneK2WwmPOLvEDh6Z7"
 
 //Include the library files
-#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
@@ -12,59 +11,53 @@
 
 #define sensor 33
 #define relay 4
+#define trig 5
+#define echo 18
+
+
+BlynkTimer timer;
 
 const int DISTANCIA_ALERTA = 30;
-
 const long TIEMPO_TAREA = 500;
 
-
 // Configuramos los pines del sensor Trigger y Echo
-const int PinTrig = 5;
-const int PinEcho = 18;
-
 noDelay pausa(TIEMPO_TAREA);
 
 // Crea una instancia de la clase NewPing
-NewPing sonar(PinTrig, PinEcho, DISTANCIA_ALERTA);
-
-//Initialize the LCD display
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-BlynkTimer timer;
+NewPing sonar(trig, echo, DISTANCIA_ALERTA);
 
 // Enter your Auth token
 char auth[] = "GwQJiB-WDTRKg5tneK2WwmPOLvEDh6Z7";
 
 //Enter your WIFI SSID and password
-char ssid[] = "plantita";
-char pass[] = "12345678";
+char ssid[] = "INFINITUMA39E_2.4";
+char pass[] = "DA6EqvGDK8";
 
 int obtenDistancia();
 
 void setup() {
   // Debug console
   Serial.begin(115200);
-  Serial.print("aaaaaa");
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
 
-  // Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  // lcd.init();
-  // lcd.backlight();
   pinMode(relay, OUTPUT);
   digitalWrite(relay, HIGH);
   // Ponemos el pin Trig en modo salida
-  pinMode(PinTrig, OUTPUT);
+  pinMode(trig, OUTPUT);
   // Ponemos el pin Echo en modo entrada
-  pinMode(PinEcho, INPUT);
+  pinMode(echo, INPUT);
 
+timer.setInterval(1000L, medirNivelAgua);
 
-  // lcd.setCursor(1, 0);
-  // lcd.print("System Loading");
-  // for (int a = 0; a <= 15; a++) {
-  //   lcd.setCursor(a, 1);
-  //   lcd.print(".");
-  //   delay(200);
-  // }
-  // lcd.clear();
+}
+
+//Get the ultrasonic sensor values
+void soilMoisture() {
+  int value = analogRead(sensor);
+  value = map(value, 0, 4095, 0, 100);
+  value = (value - 100) * -1;
+  Blynk.virtualWrite(V0, value);
+  Serial.println(value);
 }
 
 void medirNivelAgua() {
@@ -73,23 +66,14 @@ void medirNivelAgua() {
   int uS = sonar.ping_median();
   // Calcular la distancia a la que se encuentra el objeto
   int distancia = sonar.convert_cm(uS);
-  //if(distancia==DISTANCIA_ALERTA){
-  // Serial.print("Rellenar tanque");
-  //}
+  if(distancia<=DISTANCIA_ALERTA){
+    //Blynk.email("carmen.hernandez240210@potros.itson.edu.mx","Alerta","Tanque de agua bajo");
+    Blynk.logEvent("alerta_agua","El contenedor de agua, se esta quedando vacío!!! Rellénalo pronto :)");
+  Serial.print("Rellenar tanque");
+  }
+  Blynk.virtualWrite(V2, distancia);
   Serial.println(distancia);
   Serial.print("< Echo");
-  digitalWrite(relay, HIGH);
-}
-
-//Get the ultrasonic sensor values
-void soilMoisture() {
-  int value = analogRead(sensor);
-  value = map(value, 0, 4095, 0, 100);
-  value = (value - 100) * -1;
-  // Blynk.virtualWrite(V0, value);
-  Serial.println(value);
-  Serial.print("< agua");
-  digitalWrite(relay, LOW);
 }
 
 //Get the button value
@@ -97,24 +81,16 @@ BLYNK_WRITE(V1) {
   bool Relay = param.asInt();
   if (Relay == 1) {
     digitalWrite(relay, LOW);
-    // lcd.setCursor(0, 1);
-    // lcd.print("Motor is ON ");
   } else {
     digitalWrite(relay, HIGH);
-    lcd.setCursor(0, 1);
-    lcd.print("Motor is OFF");
   }
 }
 
 void loop() {
-  if (pausa.update()) {
-    soilMoisture();
-  } else {
-    medirNivelAgua();
-  }
+  soilMoisture();
+  medirNivelAgua();
+  Blynk.run();//Run the Blynk library
+  timer.run();
+  //delay(200);
 
-
-  // Blynk.run();  //Run the Blynk library
-
-  // delay(5000);
 }
