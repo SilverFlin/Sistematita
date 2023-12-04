b511543d2c4a8208cf450a3108ac590d656b4f36
SilverFlin <toledorusso@outlook.com>
2023-12-03 22:28:43 -0700

    feat: http request

--- maceta/maceta.ino
+++ maceta/maceta.ino
@@ -2,7 +2,10 @@
 #define BLYNK_TEMPLATE_NAME "Sistema de Maceta"
 #define BLYNK_AUTH_TOKEN "GwQJiB-WDTRKg5tneK2WwmPOLvEDh6Z7"
 
-//Include the library files
+// Include the library files
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
 #include <Wire.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
@@ -13,7 +16,6 @@
 #define relay 4
 #define trig 5
 #define echo 18
-
 
 BlynkTimer timer;
 
@@ -29,15 +31,19 @@ NewPing sonar(trig, echo, DISTANCIA_ALERTA);
 // Enter your Auth token
 char auth[] = "GwQJiB-WDTRKg5tneK2WwmPOLvEDh6Z7";
 
-//Enter your WIFI SSID and password
+// Enter your WIFI SSID and password
 char ssid[] = "INFINITUMA39E_2.4";
 char pass[] = "DA6EqvGDK8";
 
+#define SERVER_URL "http://192.168.100.47:5000"
+
 int obtenDistancia();
 
-void setup() {
+void setup()
+{
   // Debug console
   Serial.begin(115200);
+  connectToWiFi();
   Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
 
   pinMode(relay, OUTPUT);
@@ -47,12 +53,62 @@ void setup() {
   // Ponemos el pin Echo en modo entrada
   pinMode(echo, INPUT);
 
-timer.setInterval(1000L, medirNivelAgua);
-
+  timer.setInterval(1000L, medirNivelAgua);
+  Serial.print("Test");
+  sendPostRequest("Encendido");
 }
 
-//Get the ultrasonic sensor values
-void soilMoisture() {
+void connectToWiFi()
+{
+  // Connect to Wi-Fi
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi");
+  Serial.println("IP Address: " + WiFi.localIP().toString());
+}
+
+void sendPostRequest(const String &estado)
+{
+  HTTPClient http;
+
+  // Your server URL
+  String url = SERVER_URL;
+
+  // Create a JSON object
+  DynamicJsonDocument jsonDoc(200);
+  jsonDoc["estado"] = estado;
+
+  // Convert the JSON object to a string
+  String jsonString;
+  serializeJson(jsonDoc, jsonString);
+
+  // Start the HTTPClient
+  http.begin(url);
+
+  // Set the content type to JSON
+  http.addHeader("Content-Type", "application/json");
+
+  // Send the POST request
+  int httpResponseCode = http.POST(jsonString);
+
+  // Print the response code for debugging
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+
+  // End the request
+  http.end();
+}
+
+// Get the ultrasonic sensor values
+void soilMoisture()
+{
   int value = analogRead(sensor);
   value = map(value, 0, 4095, 0, 100);
   value = (value - 100) * -1;
@@ -60,37 +116,43 @@ void soilMoisture() {
   Serial.println(value);
 }
 
-void medirNivelAgua() {
+void medirNivelAgua()
+{
   // Obtiene la mediana de 5 mediciones del tiempo de viaje del
   // sonido entre el sensor y el objeto
   int uS = sonar.ping_median();
   // Calcular la distancia a la que se encuentra el objeto
   int distancia = sonar.convert_cm(uS);
-  if(distancia<=DISTANCIA_ALERTA){
-    //Blynk.email("carmen.hernandez240210@potros.itson.edu.mx","Alerta","Tanque de agua bajo");
-    Blynk.logEvent("alerta_agua","El contenedor de agua, se esta quedando vacío!!! Rellénalo pronto :)");
-  Serial.print("Rellenar tanque");
+  if (distancia <= DISTANCIA_ALERTA)
+  {
+    // Blynk.email("carmen.hernandez240210@potros.itson.edu.mx","Alerta","Tanque de agua bajo");
+    Blynk.logEvent("alerta_agua", "El contenedor de agua, se esta quedando vacío!!! Rellénalo pronto :)");
+    Serial.print("Rellenar tanque");
   }
   Blynk.virtualWrite(V2, distancia);
   Serial.println(distancia);
   Serial.print("< Echo");
 }
 
-//Get the button value
-BLYNK_WRITE(V1) {
+// Get the button value
+BLYNK_WRITE(V1)
+{
   bool Relay = param.asInt();
-  if (Relay == 1) {
+  if (Relay == 1)
+  {
     digitalWrite(relay, LOW);
-  } else {
+  }
+  else
+  {
     digitalWrite(relay, HIGH);
   }
 }
 
-void loop() {
+void loop()
+{
   soilMoisture();
   medirNivelAgua();
-  Blynk.run();//Run the Blynk library
+  Blynk.run(); // Run the Blynk library
   timer.run();
-  //delay(200);
-
+  // delay(200);
 }
